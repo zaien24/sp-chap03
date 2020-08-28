16dbac21901d799821fdd809a65206548074e893
zaien24 <zaien24@gmail.com>
2020-08-28 13:13:59 +0900

    # 의존 # DI를 이용해 의존 객체를 주입하는 방법 # 객체를 생성하고 의존 주입을 이용해서 객체를 서로 연결해주는 조립기 파악

---
+++ src/main/java/spring/ChangePasswordService.java
@@ -0,0 +1,22 @@
+package spring;
+
+public class ChangePasswordService {
+	// private MemberDao membeDao = new CashedMemberDao();
+	
+	private MemberDao memberDao;
+
+	public ChangePasswordService(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	public void changePassword(String email, String oldPwd, String newPwd) {
+		Member member = memberDao.selectByEmail(email);
+		if (member == null)
+			throw new MemberNotFoundException();
+		
+		member.changePassword(oldPwd, newPwd);
+		
+		memberDao.update(member);
+	}
+
+}
--- src/main/java/spring/MemberRegisterService.java
+++ src/main/java/spring/MemberRegisterService.java
@@ -1,16 +1,25 @@
 package spring;
 
-import java.lang.reflect.Member;
 import java.util.Date;
 
 public class MemberRegisterService {
 	
-	private MemberDao memberDao = new MemberDao();
+	//private MemberDao memberDao = new MemberDao();
+	// private MemberDao memberDao = new CashedMemberDao();
+	
+	private MemberDao memberDao;
+	
+	public MemberRegisterService(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
 	
 	public void regist(RegisterRequest req) {
 		Member member = memberDao.selectByEmail(req.getEmail());
+		
+		System.out.println( memberDao.selectByEmail(req.getEmail()));
+		
 		if (member != null) {
-			throw new AlreadyExistingMemberException("dup email" + req.getEmail);
+			throw new AlreadyExistingMemberException("dup email" + req.getEmail());
 		}
 				
 		Member newMember = new Member(
